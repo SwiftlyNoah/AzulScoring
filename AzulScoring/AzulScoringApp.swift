01fee32cdb85eec010dd92021e43ce37a7e3fed0
Noah Brauner <nbbrauner@gmail.com>
2022-03-25 09:41:43 -0400

    Initial Commit

---
+++ AzulScoring/AzulScoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  AzulScoringApp.swift
+//  AzulScoring
+//
+//  Created by Noah Brauner on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct AzulScoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
